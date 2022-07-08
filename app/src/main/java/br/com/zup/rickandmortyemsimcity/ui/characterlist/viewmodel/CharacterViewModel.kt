867199f99d5d12aab5f6e9b395ea7af0e2e4e37e
joanascheer <scheer.joana@gmail.com>
2022-07-08 15:22:33 -0300

    Implementando ViewModel

--- app/src/main/java/br/com/zup/rickandmortyemsimcity/ui/characterlist/viewmodel/CharacterViewModel.kt
+++ app/src/main/java/br/com/zup/rickandmortyemsimcity/ui/characterlist/viewmodel/CharacterViewModel.kt
@@ -2,7 +2,31 @@ package br.com.zup.rickandmortyemsimcity.ui.characterlist.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import br.com.zup.rickandmortyemsimcity.ERROR_VIEWSTATE_MSG
+import br.com.zup.rickandmortyemsimcity.data.model.CharacterResult
+import br.com.zup.rickandmortyemsimcity.domain.model.SingleLiveEvent
+import br.com.zup.rickandmortyemsimcity.domain.usecase.CharacterUseCase
+import br.com.zup.rickandmortyemsimcity.ui.viewstate.ViewState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class CharacterViewModel(application: Application) : AndroidViewModel(application) {
+    private val characterUseCase = CharacterUseCase()
+    val characterListState = SingleLiveEvent<ViewState<List<CharacterResult>>>()
 
+    fun getAllCharacters() {
+        viewModelScope.launch {
+            try {
+                val response = withContext(Dispatchers.IO) {
+                    characterUseCase.getAllCharactersNetwork()
+                }
+                characterListState.value = response
+            } catch (e:Exception) {
+                characterListState.value =
+                    ViewState.Error(Throwable(ERROR_VIEWSTATE_MSG))
+            }
+        }
+    }
 }
