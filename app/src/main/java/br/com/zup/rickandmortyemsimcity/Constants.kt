cf2f06a5d65c2aaf78f6c0575901d222b97f71d8
joanascheer <scheer.joana@gmail.com>
2022-07-08 15:17:00 -0300

    Implementando SingleLiveEvent

--- app/src/main/java/br/com/zup/rickandmortyemsimcity/Constants.kt
+++ app/src/main/java/br/com/zup/rickandmortyemsimcity/Constants.kt
@@ -1,3 +1,4 @@
 package br.com.zup.rickandmortyemsimcity
 
 const val ERROR_VIEWSTATE_MSG = "Não foi possível carregar a lista de personagens!"
+const val SINGLELIVEEVET_LOG_MSG = "Múltiplos observáveis registraods mas apenas um será notificado das mudanças."
