f48718dc7c2456f7ffa9ea737c4d76c2eadb24c4
joanascheer <scheer.joana@gmail.com>
2022-07-09 11:11:09 -0300

    Organizando arquivos da lista de favoritos

---
+++ app/src/main/java/br/com/zup/rickandmortyemsimcity/ui/characterfavoritelist/CharacterFavoriteListAdapter.kt
@@ -0,0 +1,47 @@
+package br.com.zup.rickandmortyemsimcity.ui.characterfavoritelist
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import br.com.zup.rickandmortyemsimcity.JPEG
+import br.com.zup.rickandmortyemsimcity.URL_BASE_IMG
+import br.com.zup.rickandmortyemsimcity.data.model.CharacterResult
+import br.com.zup.rickandmortyemsimcity.databinding.CharacterItemBinding
+import com.squareup.picasso.Picasso
+
+class CharacterFavoriteListAdapter(
+    private var characterList: MutableList<CharacterResult>,
+    private val clickCharacter: (characterResult: CharacterResult) -> Unit,
+) : RecyclerView.Adapter<CharacterFavoriteListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+            CharacterItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val character = characterList[position]
+        holder.showCharacterInfo(character)
+        holder.binding.cvItem.setOnClickListener {
+            clickCharacter(character)
+        }
+        holder.showCharacterInfo(character)
+    }
+
+    override fun getItemCount() = characterList.size
+
+    fun updateFavoriteList(newList: MutableList<CharacterResult>) {
+        characterList = newList
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder(val binding: CharacterItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun showCharacterInfo(characterResult: CharacterResult) {
+            Picasso.get().load(URL_BASE_IMG + characterResult.id + JPEG)
+                .into(binding.ivCharacter)
+            binding.tvCharacterName.text = characterResult.name
+        }
+
+    }
+}
